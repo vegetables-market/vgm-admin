ec55a5ea791229e994a1771c0a56d99d7d1480b6
yukid <yukiha617@gmail.com>
2026-01-21 04:14:09 +0900

    ページタイトルを "vgm-admin-page" に変更

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
                 <Image className="dark:invert" src="/next.svg" alt="Next.js logo" width={100} height={20} priority/>
                 <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
                     <h1 className="text-4xl font-bold tracking-tight text-black dark:text-zinc-50">
-                        vgm-admin </h1>
+                        vgm-admin-page </h1>
                     <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
                         To get started, edit the page.tsx file. </h1>
                     <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
